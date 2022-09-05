f32f19ae9fba26168e73e564ea59d792e4398d0b
arissk <skouroliakos.a@gmail.com>
2022-09-05 09:02:37 +0300

    Reports update files

--- README.md
+++
@@ -1,4 +0,0 @@
-# athensring
-
-An app that informs the user whether or not he can go downtown while the Ring restrictions are applied.
-
--- android/app/src/main/kotlin/com/example/athensring/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.athensring
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
